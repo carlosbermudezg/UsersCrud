08dc82ff1244971c6d31268d3b859e5cff9a0ab0
Carlos Bermúdez <cbermudezg7@gmail.com>
2023-03-07 00:31:55 -0500

    Json Web Token

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,7 @@
 const catchError = require('../utils/catchError');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const getAll = catchError(async(req, res) => {
     const users = await User.findAll();
@@ -15,6 +16,24 @@ const getOne = catchError(async(req, res) => {
     const { id } = req.params
     const user = await User.findByPk(id)
     return res.json(user)
+});
+const logedUser = catchError(async(req, res) => {
+    const user = req.user
+    return res.json(user)
+});
+const login = catchError(async(req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email:email }})
+    if(!user) return res.status(401).json({message: "Invalid Credentials"})
+    const isValid = await bcrypt.compare(password, user.password)
+    if(!isValid) return res.status(401).json({message: "Invalid Credentials"})
+
+    const token = jwt.sign(
+		{ user }, 
+		process.env.TOKEN_SECRET, 
+		{ expiresIn: '1d' }
+    )
+    return res.json({user, token})
 });
 const remove = catchError(async(req, res) => {
     const { id } = req.params;
@@ -34,5 +53,7 @@ module.exports = {
     create,
     getOne,
     remove,
-    update
+    update,
+    login,
+    logedUser
 }
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,13 +1,18 @@
-const { getAll, create, getOne, remove, update } = require('../controllers/users.controller');
+const { getAll, create, getOne, remove, update, login, logedUser } = require('../controllers/users.controller');
 const express = require('express');
+const verifyJWT = require('../utils/verifyJWT')
 
 const usersRouter = express.Router();
 
 usersRouter.route("/")
-		.get(getAll)
+		.get(verifyJWT, getAll)
 		.post(create)
+usersRouter.route("/login")
+		.get(login)
+usersRouter.route("/me")
+		.get(verifyJWT, logedUser)
 usersRouter.route("/:id")
-		.get(getOne)
+		.get(verifyJWT, getOne)
 		.delete(remove)
 		.put(update)
 
